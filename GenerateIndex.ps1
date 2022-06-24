7d51bb8d24aa4902ae745a97e8f0617dc794bfeb
brunotag <bruno.tagliapietra@gmail.com>
2022-06-24 18:19:19 +1200

    fix index.html generator

--- GenerateIndex.ps1
+++ GenerateIndex.ps1
@@ -1,5 +1,5 @@
-$files = Invoke-Expression -Command "dir *.tar.gz"
-New-Item './index.html' -ItemType File -Force
+$files = Get-Item ./*.tar.gz 
+New-Item './index.html' -ItemType File -Force | out-null
 
 Add-Content './index.html' '<html><head>'
 Add-Content './index.html' '<title>Lua Packages</title>'
@@ -8,9 +8,8 @@ Add-Content './index.html' '<body><h1>Lua Packages</h1>'
 
 foreach ($file in $files)
 {
-    
-    $fileName = $file -replace "\.[^\.]+$"
-    Add-Content './index.html' "<a href=""$file"">$fileName</a><br><br>"
+    $fileName = $file.Name
+    Add-Content './index.html' "<a href=""/xcsoar-lua-scripts/$fileName"">$fileName</a><br><br>"
 }
 
 Add-Content './index.html' '</body></html>'
